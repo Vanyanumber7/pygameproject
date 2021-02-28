0832df140322f581f6473dbccc83b12d4b47fb39
Smart developer <n1ckitarod@ya.ru>
2021-03-01 02:43:54 +0300

    classes Obstacle and Floor

--- dinosaur.py
+++ dinosaur.py
@@ -1,7 +1,28 @@
+import os
+import sys
+from random import choice
+
 import pygame
 
 
-class Dinosaurs(pygame.sprite.Sprite):
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Dinosaur(pygame.sprite.Sprite):
     def __init__(self, sheet, columns, rows, x, y, surfaceHeight, all_sprites):
         super().__init__(all_sprites)
         self.frames = []
@@ -35,7 +56,7 @@ class Dinosaurs(pygame.sprite.Sprite):
     def update(self, obstacles):
         if pygame.sprite.spritecollideany(self, obstacles):
             quit()
-        self.yvelocity += -500 // 15 * 1.5# Gravity
+        self.yvelocity += -500 // 15 * 1.5  # Gravity
         self.y += self.yvelocity // 15 * 1.5
         if self.y <= 0:
             self.y = 0
@@ -43,3 +64,67 @@ class Dinosaurs(pygame.sprite.Sprite):
             self.cur_frame = (self.cur_frame + 1) % len(self.frames)
             self.image = self.frames[self.cur_frame]
         self.rect.y = self.surfaceHeight - self.y - self.rect.y
+
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, size, GroundHeight, all_sprites):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.image = choice(self.frames)
+        self.size = size
+        self.GroundHeight = GroundHeight
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = self.GroundHeight - self.size
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.rect.x -= 20
+
+    def checkOver(self):
+        if self.rect.x <= 0:
+            return True
+        else:
+            return False
+
+
+class Floor(pygame.sprite.Sprite):
+    def __init__(self, sheet, x, GroundHeight, all_sprites):
+        super().__init__(all_sprites)
+        self.all_sprites = all_sprites
+        self.image = sheet
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = GroundHeight - sheet.get_height()
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        if self.rect.x <= -self.image.get_width():
+            self.kill()
+            return False
+        elif -self.image.get_width() + 680 <= self.rect.x < -self.image.get_width() + 700:
+            self.rect.x -= 20
+            return True
+        else:
+            self.rect.x -= 20
+            return False
+
+    def checkOver(self):
+        return False
